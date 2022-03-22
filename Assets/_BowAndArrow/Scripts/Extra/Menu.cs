6c04213a39f37f840ebe45e9fa7841d24a3599a8
antolefou <simon72610@gmail.com>
2022-03-22 09:48:31 -0400

    add menu

--- Assets/_BowAndArrow/Scripts/Extra/Menu.cs
+++ Assets/_BowAndArrow/Scripts/Extra/Menu.cs
@@ -1,18 +1,21 @@
-﻿using TMPro;
+using TMPro;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
-public class Target : MonoBehaviour, IArrowHittable
+public class Menu : MonoBehaviour, IArrowHittable
 {
     public float forceAmount = 1.0f;
     public Material otherMaterial = null;
-    public int score = 1;
+    public string scene;
 
     public void Hit(Arrow arrow)
     {
         ApplyMaterial();
         ApplyForce(arrow.transform.forward);
-        Player.SetScorePlayer(score);
-        GameObject.Find("AffichageScore").GetComponent<TextMeshPro>().text = Player.GetScorePlayer().ToString();
+        if (scene != null)
+        {
+            SceneManager.LoadScene(scene, LoadSceneMode.Single);
+        }
     }
 
     private void ApplyMaterial()
